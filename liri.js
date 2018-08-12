0be9df69a33c90e918b40dcf9d2c84f38c81e064
jamesngo125 <37889383+jamesngo125@users.noreply.github.com>
2018-08-12 13:34:46 -0700

    Update liri.js

--- liri.js
+++ liri.js
@@ -1,1 +1,14 @@
 
+require("dotenv").congif();
+var Twitter = require('twitter');
+var Spotify = require('node-spotify-api');
+var Request = require('request');
+var fs = require('fs');
+var keys = require('./keys');
+var spotifyKey = new Spotify(keys.spotify);
+var twitterKey = new Twitter(keys.twitter);
+var args = process.argv.slice(2);
+var command = args[0];
+var search = args[1];
+function myTweets() {
+    var params = {
